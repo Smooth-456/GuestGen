f4228f08c95c3d546baccf6428fb5090705ff226
Smooth-456 <dhanlaxmim4@gmail.com>
2026-08-02 19:06:08 +0530

    Modify start.sh to include keep_alive.py

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# Start the web server to keep Render awake
+python keep_alive.py &
+
+# Start gen.py in the background
 python gen.py &
+
+# Start act.py in the foreground
 python act.py
